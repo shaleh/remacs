0957a8f06b43c24951ee21f1f2ebbe1380c5afcf
David DeSimone <djd231@scarletmail.rutgers.edu>
2017-03-06 00:34:16 -0800

    Updating lib-src source code to use new rust_make_temp function instead of mkostemp

--- lib-src/update-game-score.c
+++ lib-src/update-game-score.c
@@ -52,6 +52,8 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 #include "ntlib.h"
 #endif
 
+#include "rust-temp.h"
+
 #ifndef min
 # define min(a,b) ((a) < (b) ? (a) : (b))
 #endif
@@ -432,11 +434,14 @@ write_scores (const char *filename, mode_t mode,
   int fd;
   FILE *f;
   ptrdiff_t i;
+  int errcode;
   char *tempfile = malloc (strlen (filename) + strlen (".tempXXXXXX") + 1);
   if (!tempfile)
     return -1;
   strcpy (stpcpy (tempfile, filename), ".tempXXXXXX");
-  fd = mkostemp (tempfile, 0);
+  fd = rust_make_temp (tempfile, 0, &errcode);
+  if (errcode != 0)
+    errno = errcode;
   if (fd < 0)
     return -1;
 #ifndef DOS_NT
