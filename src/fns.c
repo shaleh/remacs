2112a888013d448ea3aec50ee55cc43dd61e8288
Georg Brandl <georg@python.org>
2017-05-25 17:53:57 +0200

    Move the following Lisp functions to Rust:
    * eql
    * equal
    * equal-including-properties
    * delq
    * car-safe
    * cdr-safe
    * memq
    * memql
    * member
    * nthcdr
    * nth
    * assq
    * assoc
    * rassq
    * rassoc
    * plist-get
    * plist-put
    * lax-plist-put
    * lax-plist-get
    * plist-member
    * string-equal
    * markerp
    
    Other changes:
    
    * Create comparison methods for LispObject for each of the three
      ELisp ways (eq, eql, equal).
    * Use the provided way to construct bools and t.
    * Create a Rusty API for working with cons cells (as_cons() returns
      a newtype that has car() and cdr() methods).
    * Introduce a list tails iterator to replace the FOR_EACH_TAIL and
      FOR_EACH_TAIL_SAFE macro.
    * Add missing Cargo.lock for remacs-sys.
    * Migrate XXXP() functions to LispObject::is_xxx() and remove unused ones.
    * Avoid allocating a vector for each call to a MANY function.
    * Fixup makefile targets to avoid "override" warnings.

--- src/fns.c
+++ src/fns.c
@@ -43,9 +43,7 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 
 static void sort_vector_copy (Lisp_Object, ptrdiff_t,
 			      Lisp_Object *restrict, Lisp_Object *restrict);
-enum equal_kind { EQUAL_NO_QUIT, EQUAL_PLAIN, EQUAL_INCLUDING_PROPERTIES };
-static bool internal_equal (Lisp_Object, Lisp_Object,
-			    enum equal_kind, int, Lisp_Object);
+bool internal_equal (Lisp_Object, Lisp_Object, int, bool, Lisp_Object);
 
 DEFUN ("identity", Fidentity, Sidentity, 1, 1, 0,
        doc: /* Return the argument unchanged.  */
@@ -142,26 +140,6 @@ which is at least the number of distinct elements.  */)
   FOR_EACH_TAIL_SAFE (list)
     len++;
   return make_fixnum_or_float (len);
-}
-
-DEFUN ("string-equal", Fstring_equal, Sstring_equal, 2, 2, 0,
-       doc: /* Return t if two strings have identical contents.
-Case is significant, but text properties are ignored.
-Symbols are also allowed; their print names are used instead.  */)
-  (register Lisp_Object s1, Lisp_Object s2)
-{
-  if (SYMBOLP (s1))
-    s1 = SYMBOL_NAME (s1);
-  if (SYMBOLP (s2))
-    s2 = SYMBOL_NAME (s2);
-  CHECK_STRING (s1);
-  CHECK_STRING (s2);
-
-  if (SCHARS (s1) != SCHARS (s2)
-      || SBYTES (s1) != SBYTES (s2)
-      || memcmp (SDATA (s1), SDATA (s2), SBYTES (s1)))
-    return Qnil;
-  return Qt;
 }
 
 DEFUN ("compare-strings", Fcompare_strings, Scompare_strings, 6, 7, 0,
@@ -1301,33 +1279,6 @@ substring_both (Lisp_Object string, ptrdiff_t from, ptrdiff_t from_byte,
 
   return res;
 }
-
-DEFUN ("nthcdr", Fnthcdr, Snthcdr, 2, 2, 0,
-       doc: /* Take cdr N times on LIST, return the result.  */)
-  (Lisp_Object n, Lisp_Object list)
-{
-  CHECK_NUMBER (n);
-  Lisp_Object tail = list;
-  for (EMACS_INT num = XINT (n); 0 < num; num--)
-    {
-      if (! CONSP (tail))
-	{
-	  CHECK_LIST_END (tail, list);
-	  return Qnil;
-	}
-      tail = XCDR (tail);
-      rarely_quit (num);
-    }
-  return tail;
-}
-
-DEFUN ("nth", Fnth, Snth, 2, 2, 0,
-       doc: /* Return the Nth element of LIST.
-N counts from zero.  If LIST is not that long, nil is returned.  */)
-  (Lisp_Object n, Lisp_Object list)
-{
-  return Fcar (Fnthcdr (n, list));
-}
 
 DEFUN ("elt", Felt, Selt, 2, 2, 0,
        doc: /* Return element of SEQUENCE at index N.  */)
@@ -1342,65 +1293,6 @@ DEFUN ("elt", Felt, Selt, 2, 2, 0,
   return Faref (sequence, n);
 }
 
-DEFUN ("member", Fmember, Smember, 2, 2, 0,
-       doc: /* Return non-nil if ELT is an element of LIST.  Comparison done with `equal'.
-The value is actually the tail of LIST whose car is ELT.  */)
-  (Lisp_Object elt, Lisp_Object list)
-{
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    if (! NILP (Fequal (elt, XCAR (tail))))
-      return tail;
-  CHECK_LIST_END (tail, list);
-  return Qnil;
-}
-
-DEFUN ("memq", Fmemq, Smemq, 2, 2, 0,
-       doc: /* Return non-nil if ELT is an element of LIST.  Comparison done with `eq'.
-The value is actually the tail of LIST whose car is ELT.  */)
-  (Lisp_Object elt, Lisp_Object list)
-{
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    if (EQ (XCAR (tail), elt))
-      return tail;
-  CHECK_LIST_END (tail, list);
-  return Qnil;
-}
-
-DEFUN ("memql", Fmemql, Smemql, 2, 2, 0,
-       doc: /* Return non-nil if ELT is an element of LIST.  Comparison done with `eql'.
-The value is actually the tail of LIST whose car is ELT.  */)
-  (Lisp_Object elt, Lisp_Object list)
-{
-  if (!FLOATP (elt))
-    return Fmemq (elt, list);
-
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    {
-      Lisp_Object tem = XCAR (tail);
-      if (FLOATP (tem) && equal_no_quit (elt, tem))
-	return tail;
-    }
-  CHECK_LIST_END (tail, list);
-  return Qnil;
-}
-
-DEFUN ("assq", Fassq, Sassq, 2, 2, 0,
-       doc: /* Return non-nil if KEY is `eq' to the car of an element of LIST.
-The value is actually the first element of LIST whose car is KEY.
-Elements of LIST that are not conses are ignored.  */)
-  (Lisp_Object key, Lisp_Object list)
-{
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    if (CONSP (XCAR (tail)) && EQ (XCAR (XCAR (tail)), key))
-      return XCAR (tail);
-  CHECK_LIST_END (tail, list);
-  return Qnil;
-}
-
 /* Like Fassq but never report an error and do not allow quits.
    Use only on objects known to be non-circular lists.  */
 
@@ -1410,28 +1302,6 @@ assq_no_quit (Lisp_Object key, Lisp_Object list)
   for (; ! NILP (list); list = XCDR (list))
     if (CONSP (XCAR (list)) && EQ (XCAR (XCAR (list)), key))
       return XCAR (list);
-  return Qnil;
-}
-
-DEFUN ("assoc", Fassoc, Sassoc, 2, 3, 0,
-       doc: /* Return non-nil if KEY is equal to the car of an element of LIST.
-The value is actually the first element of LIST whose car equals KEY.
-
-Equality is defined by TESTFN if non-nil or by `equal' if nil.  */)
-     (Lisp_Object key, Lisp_Object list, Lisp_Object testfn)
-{
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    {
-      Lisp_Object car = XCAR (tail);
-      if (CONSP (car)
-	  && (NILP (testfn)
-	      ? (EQ (XCAR (car), key) || !NILP (Fequal
-						(XCAR (car), key)))
-	      : !NILP (call2 (testfn, XCAR (car), key))))
-	return car;
-    }
-  CHECK_LIST_END (tail, list);
   return Qnil;
 }
 
@@ -1450,67 +1320,6 @@ assoc_no_quit (Lisp_Object key, Lisp_Object list)
 	return car;
     }
   return Qnil;
-}
-
-DEFUN ("rassq", Frassq, Srassq, 2, 2, 0,
-       doc: /* Return non-nil if KEY is `eq' to the cdr of an element of LIST.
-The value is actually the first element of LIST whose cdr is KEY.  */)
-  (Lisp_Object key, Lisp_Object list)
-{
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    if (CONSP (XCAR (tail)) && EQ (XCDR (XCAR (tail)), key))
-      return XCAR (tail);
-  CHECK_LIST_END (tail, list);
-  return Qnil;
-}
-
-DEFUN ("rassoc", Frassoc, Srassoc, 2, 2, 0,
-       doc: /* Return non-nil if KEY is `equal' to the cdr of an element of LIST.
-The value is actually the first element of LIST whose cdr equals KEY.  */)
-  (Lisp_Object key, Lisp_Object list)
-{
-  Lisp_Object tail = list;
-  FOR_EACH_TAIL (tail)
-    {
-      Lisp_Object car = XCAR (tail);
-      if (CONSP (car)
-	  && (EQ (XCDR (car), key) || !NILP (Fequal (XCDR (car), key))))
-	return car;
-    }
-  CHECK_LIST_END (tail, list);
-  return Qnil;
-}
-
-DEFUN ("delq", Fdelq, Sdelq, 2, 2, 0,
-       doc: /* Delete members of LIST which are `eq' to ELT, and return the result.
-More precisely, this function skips any members `eq' to ELT at the
-front of LIST, then removes members `eq' to ELT from the remaining
-sublist by modifying its list structure, then returns the resulting
-list.
-
-Write `(setq foo (delq element foo))' to be sure of correctly changing
-the value of a list `foo'.  See also `remq', which does not modify the
-argument.  */)
-  (Lisp_Object elt, Lisp_Object list)
-{
-  Lisp_Object prev = Qnil, tail = list;
-
-  FOR_EACH_TAIL (tail)
-    {
-      Lisp_Object tem = XCAR (tail);
-      if (EQ (elt, tem))
-	{
-	  if (NILP (prev))
-	    list = XCDR (tail);
-	  else
-	    Fsetcdr (prev, XCDR (tail));
-	}
-      else
-	prev = tail;
-    }
-  CHECK_LIST_END (tail, list);
-  return list;
 }
 
 DEFUN ("delete", Fdelete, Sdelete, 2, 2, 0,
@@ -1947,30 +1756,6 @@ merge (Lisp_Object org_l1, Lisp_Object org_l2, Lisp_Object pred)
 }
 
 
-/* This does not check for quits.  That is safe since it must terminate.  */
-
-DEFUN ("plist-get", Fplist_get, Splist_get, 2, 2, 0,
-       doc: /* Extract a value from a property list.
-PLIST is a property list, which is a list of the form
-\(PROP1 VALUE1 PROP2 VALUE2...).  This function returns the value
-corresponding to the given PROP, or nil if PROP is not one of the
-properties on the list.  This function never signals an error.  */)
-  (Lisp_Object plist, Lisp_Object prop)
-{
-  Lisp_Object tail = plist;
-  FOR_EACH_TAIL_SAFE (tail)
-    {
-      if (! CONSP (XCDR (tail)))
-	break;
-      if (EQ (prop, XCAR (tail)))
-	return XCAR (XCDR (tail));
-      tail = XCDR (tail);
-      if (EQ (tail, li.tortoise))
-	break;
-    }
-
-  return Qnil;
-}
 
 DEFUN ("get", Fget, Sget, 2, 2, 0,
        doc: /* Return the value of SYMBOL's PROPNAME property.
@@ -1985,42 +1770,6 @@ This is the last value stored with `(put SYMBOL PROPNAME VALUE)'.  */)
   return Fplist_get (XSYMBOL (symbol)->u.s.plist, propname);
 }
 
-DEFUN ("plist-put", Fplist_put, Splist_put, 3, 3, 0,
-       doc: /* Change value in PLIST of PROP to VAL.
-PLIST is a property list, which is a list of the form
-\(PROP1 VALUE1 PROP2 VALUE2 ...).  PROP is a symbol and VAL is any object.
-If PROP is already a property on the list, its value is set to VAL,
-otherwise the new PROP VAL pair is added.  The new plist is returned;
-use `(setq x (plist-put x prop val))' to be sure to use the new value.
-The PLIST is modified by side effects.  */)
-  (Lisp_Object plist, Lisp_Object prop, Lisp_Object val)
-{
-  Lisp_Object prev = Qnil, tail = plist;
-  FOR_EACH_TAIL (tail)
-    {
-      if (! CONSP (XCDR (tail)))
-	break;
-
-      if (EQ (prop, XCAR (tail)))
-	{
-	  Fsetcar (XCDR (tail), val);
-	  return plist;
-	}
-
-      prev = tail;
-      tail = XCDR (tail);
-      if (EQ (tail, li.tortoise))
-	circular_list (plist);
-    }
-  CHECK_TYPE (NILP (tail), Qplistp, plist);
-  Lisp_Object newcell
-    = Fcons (prop, Fcons (val, NILP (prev) ? plist : XCDR (XCDR (prev))));
-  if (NILP (prev))
-    return newcell;
-  Fsetcdr (XCDR (prev), newcell);
-  return plist;
-}
-
 DEFUN ("put", Fput, Sput, 3, 3, 0,
        doc: /* Store SYMBOL's PROPNAME property with value VALUE.
 It can be retrieved with `(get SYMBOL PROPNAME)'.  */)
@@ -2030,110 +1779,6 @@ It can be retrieved with `(get SYMBOL PROPNAME)'.  */)
   set_symbol_plist
     (symbol, Fplist_put (XSYMBOL (symbol)->u.s.plist, propname, value));
   return value;
-}
-
-DEFUN ("lax-plist-get", Flax_plist_get, Slax_plist_get, 2, 2, 0,
-       doc: /* Extract a value from a property list, comparing with `equal'.
-PLIST is a property list, which is a list of the form
-\(PROP1 VALUE1 PROP2 VALUE2...).  This function returns the value
-corresponding to the given PROP, or nil if PROP is not
-one of the properties on the list.  */)
-  (Lisp_Object plist, Lisp_Object prop)
-{
-  Lisp_Object tail = plist;
-  FOR_EACH_TAIL (tail)
-    {
-      if (! CONSP (XCDR (tail)))
-	break;
-      if (! NILP (Fequal (prop, XCAR (tail))))
-	return XCAR (XCDR (tail));
-      tail = XCDR (tail);
-      if (EQ (tail, li.tortoise))
-	circular_list (plist);
-    }
-
-  CHECK_TYPE (NILP (tail), Qplistp, plist);
-
-  return Qnil;
-}
-
-DEFUN ("lax-plist-put", Flax_plist_put, Slax_plist_put, 3, 3, 0,
-       doc: /* Change value in PLIST of PROP to VAL, comparing with `equal'.
-PLIST is a property list, which is a list of the form
-\(PROP1 VALUE1 PROP2 VALUE2 ...).  PROP and VAL are any objects.
-If PROP is already a property on the list, its value is set to VAL,
-otherwise the new PROP VAL pair is added.  The new plist is returned;
-use `(setq x (lax-plist-put x prop val))' to be sure to use the new value.
-The PLIST is modified by side effects.  */)
-  (Lisp_Object plist, Lisp_Object prop, Lisp_Object val)
-{
-  Lisp_Object prev = Qnil, tail = plist;
-  FOR_EACH_TAIL (tail)
-    {
-      if (! CONSP (XCDR (tail)))
-	break;
-
-      if (! NILP (Fequal (prop, XCAR (tail))))
-	{
-	  Fsetcar (XCDR (tail), val);
-	  return plist;
-	}
-
-      prev = tail;
-      tail = XCDR (tail);
-      if (EQ (tail, li.tortoise))
-	circular_list (plist);
-    }
-  CHECK_TYPE (NILP (tail), Qplistp, plist);
-  Lisp_Object newcell = list2 (prop, val);
-  if (NILP (prev))
-    return newcell;
-  Fsetcdr (XCDR (prev), newcell);
-  return plist;
-}
-
-DEFUN ("eql", Feql, Seql, 2, 2, 0,
-       doc: /* Return t if the two args are the same Lisp object.
-Floating-point numbers of equal value are `eql', but they may not be `eq'.  */)
-  (Lisp_Object obj1, Lisp_Object obj2)
-{
-  if (FLOATP (obj1))
-    return equal_no_quit (obj1, obj2) ? Qt : Qnil;
-  else
-    return EQ (obj1, obj2) ? Qt : Qnil;
-}
-
-DEFUN ("equal", Fequal, Sequal, 2, 2, 0,
-       doc: /* Return t if two Lisp objects have similar structure and contents.
-They must have the same data type.
-Conses are compared by comparing the cars and the cdrs.
-Vectors and strings are compared element by element.
-Numbers are compared by value, but integers cannot equal floats.
- (Use `=' if you want integers and floats to be able to be equal.)
-Symbols must match exactly.  */)
-  (Lisp_Object o1, Lisp_Object o2)
-{
-  return internal_equal (o1, o2, EQUAL_PLAIN, 0, Qnil) ? Qt : Qnil;
-}
-
-DEFUN ("equal-including-properties", Fequal_including_properties, Sequal_including_properties, 2, 2, 0,
-       doc: /* Return t if two Lisp objects have similar structure and contents.
-This is like `equal' except that it compares the text properties
-of strings.  (`equal' ignores text properties.)  */)
-  (Lisp_Object o1, Lisp_Object o2)
-{
-  return (internal_equal (o1, o2, EQUAL_INCLUDING_PROPERTIES, 0, Qnil)
-	  ? Qt : Qnil);
-}
-
-/* Return true if O1 and O2 are equal.  Do not quit or check for cycles.
-   Use this only on arguments that are cycle-free and not too large and
-   are not window configurations.  */
-
-bool
-equal_no_quit (Lisp_Object o1, Lisp_Object o2)
-{
-  return internal_equal (o1, o2, EQUAL_NO_QUIT, 0, Qnil);
 }
 
 /* Return true if O1 and O2 are equal.  EQUAL_KIND specifies what kind
@@ -2147,9 +1792,10 @@ equal_no_quit (Lisp_Object o1, Lisp_Object o2)
    it has not been allocated yet.  But ignore the last two arguments
    if EQUAL_KIND == EQUAL_NO_QUIT.  */
 
-static bool
-internal_equal (Lisp_Object o1, Lisp_Object o2, enum equal_kind equal_kind,
-		int depth, Lisp_Object ht)
+/* NOTE: made this non-static to call it from Rust. */
+bool
+internal_equal (Lisp_Object o1, Lisp_Object o2, int depth, bool props,
+		Lisp_Object ht)
 {
  tail_recurse:
   if (depth > 10)
@@ -2832,30 +2478,6 @@ suppressed.  */)
    out that some functions in the widget library (wid-edit.el) are the
    bottleneck of Widget operation.  Here is their translation to C,
    for the sole reason of efficiency.  */
-
-DEFUN ("plist-member", Fplist_member, Splist_member, 2, 2, 0,
-       doc: /* Return non-nil if PLIST has the property PROP.
-PLIST is a property list, which is a list of the form
-\(PROP1 VALUE1 PROP2 VALUE2 ...).  PROP is a symbol.
-Unlike `plist-get', this allows you to distinguish between a missing
-property and a property with the value nil.
-The value is actually the tail of PLIST whose car is PROP.  */)
-  (Lisp_Object plist, Lisp_Object prop)
-{
-  Lisp_Object tail = plist;
-  FOR_EACH_TAIL (tail)
-    {
-      if (EQ (XCAR (tail), prop))
-	return tail;
-      tail = XCDR (tail);
-      if (! CONSP (tail))
-	break;
-      if (EQ (tail, li.tortoise))
-	circular_list (tail);
-    }
-  CHECK_TYPE (NILP (tail), Qplistp, plist);
-  return Qnil;
-}
 
 DEFUN ("widget-put", Fwidget_put, Swidget_put, 3, 3, 0,
        doc: /* In WIDGET, set PROPERTY to VALUE.
@@ -4890,7 +4512,6 @@ this variable.  */);
   defsubr (&Srandom);
   defsubr (&Slength);
   defsubr (&Ssafe_length);
-  defsubr (&Sstring_equal);
   defsubr (&Scompare_strings);
   defsubr (&Sstring_lessp);
   defsubr (&Sstring_version_lessp);
@@ -4909,30 +4530,13 @@ this variable.  */);
   defsubr (&Scopy_alist);
   defsubr (&Ssubstring);
   defsubr (&Ssubstring_no_properties);
-  defsubr (&Snthcdr);
-  defsubr (&Snth);
   defsubr (&Selt);
-  defsubr (&Smember);
-  defsubr (&Smemq);
-  defsubr (&Smemql);
-  defsubr (&Sassq);
-  defsubr (&Sassoc);
-  defsubr (&Srassq);
-  defsubr (&Srassoc);
-  defsubr (&Sdelq);
   defsubr (&Sdelete);
   defsubr (&Snreverse);
   defsubr (&Sreverse);
   defsubr (&Ssort);
-  defsubr (&Splist_get);
   defsubr (&Sget);
-  defsubr (&Splist_put);
   defsubr (&Sput);
-  defsubr (&Slax_plist_get);
-  defsubr (&Slax_plist_put);
-  defsubr (&Seql);
-  defsubr (&Sequal);
-  defsubr (&Sequal_including_properties);
   defsubr (&Sfillarray);
   defsubr (&Sclear_string);
   defsubr (&Snconc);
@@ -4945,7 +4549,6 @@ this variable.  */);
   defsubr (&Sfeaturep);
   defsubr (&Srequire);
   defsubr (&Sprovide);
-  defsubr (&Splist_member);
   defsubr (&Swidget_put);
   defsubr (&Swidget_get);
   defsubr (&Swidget_apply);
