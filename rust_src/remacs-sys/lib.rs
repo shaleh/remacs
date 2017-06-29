2465bc89f653c0272ef9d103829b1ce4af377cb5
David DeSimone <david.desimone@bigfishgames.com>
2017-06-28 18:55:23 -0700

    Laying the groundwork for representing a lisp symbol as a rust struct. This will allow us to create a LispSymbolRef like we have a LispStringRef. This will also allow a similar API for working with symbols as we have for strings.

--- rust_src/remacs-sys/lib.rs
+++ rust_src/remacs-sys/lib.rs
@@ -664,6 +664,11 @@ pub struct Lisp_String {
     pub data: *mut libc::c_char,
 }
 
+// @TODO
+#[repr(C)]
+pub struct Lisp_Symbol {
+}
+
 extern "C" {
     pub static mut globals: emacs_globals;
     pub static Qt: Lisp_Object;
@@ -674,6 +679,7 @@ extern "C" {
     pub static Qintegerp: Lisp_Object;
     pub static Qfloatp: Lisp_Object;
     pub static Qstringp: Lisp_Object;
+    pub static Qsymbolp: Lisp_Object;
     pub static Qlistp: Lisp_Object;
     pub static Qmarkerp: Lisp_Object;
     pub static Qwholenump: Lisp_Object;
