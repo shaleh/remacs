5ef1a84838280078d1e7f79eb1a60060b984a490
Wilfred Hughes <me@wilfred.me.uk>
2017-07-30 20:22:59 +0100

    Define buffer-file-name in Rust

--- rust_src/remacs-sys/lib.rs
+++ rust_src/remacs-sys/lib.rs
@@ -1295,6 +1295,7 @@ extern "C" {
     pub static Qsequencep: Lisp_Object;
     pub static Qcharacterp: Lisp_Object;
     pub static Qchar_table_p: Lisp_Object;
+    pub static Qbufferp: Lisp_Object;
     pub static Qminus: Lisp_Object;
 
     pub static Qinteger: Lisp_Object;
@@ -1343,7 +1344,7 @@ extern "C" {
     pub fn Fcons(car: Lisp_Object, cdr: Lisp_Object) -> Lisp_Object;
     pub fn Fcurrent_buffer() -> Lisp_Object;
     pub fn Fsignal(error_symbol: Lisp_Object, data: Lisp_Object) -> !;
-    pub fn Fbuffer_file_name(buffer: Lisp_Object) -> Lisp_Object;
+    pub fn Fcopy_sequence(seq: Lisp_Object) -> Lisp_Object;
     pub fn Ffind_operation_coding_system(nargs: ptrdiff_t, args: *mut Lisp_Object) -> Lisp_Object;
     pub fn Flocal_variable_p(variable: Lisp_Object, buffer: Lisp_Object) -> Lisp_Object;
 
--- rust_src/src/crypto/mod.rs
+++ rust_src/src/crypto/mod.rs
@@ -5,8 +5,7 @@ use std;
 use std::slice;
 use libc::ptrdiff_t;
 
-use buffers::{LispBufferRef, get_buffer};
-use eval::xsignal1;
+use buffers::{LispBufferRef, get_buffer, buffer_file_name};
 use libc;
 use lisp::{LispObject, LispNumber};
 use multibyte::LispStringRef;
@@ -14,8 +13,8 @@ use remacs_sys::{nsberror, Fcurrent_buffer, EmacsInt, make_uninit_string, make_s
 use remacs_sys::{preferred_coding_system, Fcoding_system_p, code_convert_string,
                  validate_subarray, string_char_to_byte, wrong_type_argument};
 use remacs_sys::{current_thread, record_unwind_current_buffer, set_buffer_internal,
-                 make_buffer_string, call4};
-use remacs_sys::{globals, Fbuffer_file_name, Ffind_operation_coding_system, Flocal_variable_p};
+                 make_buffer_string};
+use remacs_sys::{globals, Ffind_operation_coding_system, Flocal_variable_p};
 use remacs_sys::{Qmd5, Qsha1, Qsha224, Qsha256, Qsha384, Qsha512, Qstringp, Qraw_text,
                  Qcoding_system_error, Qwrite_region, Qbuffer_file_coding_system};
 use remacs_macros::lisp_fn;
@@ -118,13 +117,13 @@ fn get_coding_system_for_buffer(
             return LispObject::from_raw(unsafe { Qraw_text });
         }
     }
-    if LispObject::from_raw(unsafe { Fbuffer_file_name(object.to_raw()) }).is_not_nil() {
+    if buffer_file_name(object).is_not_nil() {
         /* Check file-coding-system-alist. */
         let mut args = [
             unsafe { Qwrite_region },
             start.to_raw(),
             end.to_raw(),
-            unsafe { Fbuffer_file_name(object.to_raw()) },
+            buffer_file_name(object).to_raw(),
         ];
         let val = LispObject::from_raw(unsafe {
             Ffind_operation_coding_system(4, args.as_mut_ptr())
