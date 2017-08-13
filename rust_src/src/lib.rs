485daba68d6c43c1605aae51c7b3df1c436caf33
Shanavas M <shanavas@disroot.org>
2017-08-13 11:12:33 +0300

    Port buffer-modified-tick and buffer-chars-modified-tick to rust

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -171,6 +171,8 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*buffers::Scurrent_buffer);
         defsubr(&*buffers::Sbuffer_file_name);
         defsubr(&*buffers::Sbuffer_modified_p);
+        defsubr(&*buffers::Sbuffer_modified_tick);
+        defsubr(&*buffers::Sbuffer_chars_modified_tick);
         defsubr(&*buffers::Sbuffer_name);
         defsubr(&*windows::Swindowp);
         defsubr(&*process::Sget_process);
