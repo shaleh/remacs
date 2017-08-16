a6e24c0e3707ac10bd6340462feae72e1dabcbf1
Raimon Grau <raimonster@gmail.com>
2017-08-16 15:56:55 +0200

    Port bolp to rust

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -158,6 +158,7 @@ pub use multibyte::str_to_unibyte;
 
 // Used in window.c, macros.c
 pub use interactive::Fprefix_numeric_value;
+pub use editfns::Fbolp;
 
 extern "C" {
     fn defsubr(sname: *const Lisp_Subr);
@@ -310,5 +311,6 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*editfns::Sbuffer_size);
         defsubr(&*editfns::Seobp);
         defsubr(&*editfns::Sbobp);
+        defsubr(&*editfns::Sbolp);
     }
 }
