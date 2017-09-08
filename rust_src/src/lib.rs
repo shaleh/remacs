6e76a2b461955793dfcc7915d0ad8cf876e1ab65
brotzeit <brotzeitmacher@gmail.com>
2017-09-08 16:51:34 +0200

    port window-valid-p

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -207,6 +207,7 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*windows::Swindow_point);
         defsubr(&*windows::Sselected_window);
         defsubr(&*windows::Swindow_buffer);
+        defsubr(&*windows::Swindow_valid_p);
         defsubr(&*process::Sget_process);
         defsubr(&*process::Sprocessp);
         defsubr(&*lists::Satom);
