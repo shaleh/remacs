5fe8d65854d1d73373b35badae80425c41f5dade
Shanavas M <shanavas@disroot.org>
2017-09-18 15:13:03 +0300

    Port fmakunbound to rust

--- rust_src/remacs-sys/lib.rs
+++ rust_src/remacs-sys/lib.rs
@@ -1343,6 +1343,8 @@ extern "C" {
     pub static Qfont_object: Lisp_Object;
     pub static Qwrite_region: Lisp_Object;
     pub static Qbuffer_file_coding_system: Lisp_Object;
+    pub static Qfont_extra_type: Lisp_Object;
+    pub static Qsetting_constant: Lisp_Object;
 
     pub static Qmd5: Lisp_Object;
     pub static Qsha1: Lisp_Object;
