86751d8e79c659f7c6a4689597218c944ed055a5
Raimon Grau <raimonster@gmail.com>
2017-09-25 13:31:26 +0100

    Port identity function to rust

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -196,6 +196,9 @@ pub use editfns::Feolp;
 // Used in minibuffer.c
 pub use windows::Fwindow_minibuffer_p;
 
+// Used in term.c, dired.c
+pub use objects::Fidentity;
+
 extern "C" {
     fn defsubr(sname: *const Lisp_Subr);
 }
@@ -262,6 +265,7 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*objects::Seql);
         defsubr(&*objects::Sequal);
         defsubr(&*objects::Sequal_including_properties);
+        defsubr(&*objects::Sidentity);
         defsubr(&*symbols::Ssymbolp);
         defsubr(&*symbols::Ssymbol_name);
         defsubr(&*symbols::Sfboundp);
@@ -372,6 +376,6 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*editfns::Sbobp);
         defsubr(&*minibuf::Sminibufferp);
         defsubr(&*minibuf::Sactive_minibuffer_window);
-        defsubr(&*cmds::Sforward_point)
+        defsubr(&*cmds::Sforward_point);
     }
 }
