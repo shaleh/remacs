95679cdff29f31627d5f0c66b0a585cf29c2ac15
Sean Perry <shaleh@speakeasy.net>
2017-10-26 01:25:41 -0700

    Port current-column (#392)
    * Port current-column

--- rust_src/remacs-sys/lib.rs
+++ rust_src/remacs-sys/lib.rs
@@ -1168,7 +1168,29 @@ extern "C" {
     pub fn set_point_both(charpos: ptrdiff_t, bytepos: ptrdiff_t);
     pub fn buf_charpos_to_bytepos(buffer: *const Lisp_Buffer, charpos: ptrdiff_t) -> ptrdiff_t;
 
-    // textprop
+    pub fn Finsert_char(
+        character: Lisp_Object,
+        count: Lisp_Object,
+        inherit: Lisp_Object,
+    ) -> Lisp_Object;
+
+    pub fn wait_reading_process_output(
+        time_limit: intmax_t,
+        nsecs: c_int,
+        read_kbd: c_int,
+        do_display: bool,
+        wait_for_cell: Lisp_Object,
+        wait_proc: *const Lisp_Process,
+        just_wait_proc: c_int,
+    ) -> c_int;
+
+    pub fn dtotimespec(sec: c_double) -> timespec;
+    pub fn current_timespec() -> timespec;
+    pub fn timespec_sub(a: timespec, b: timespec) -> timespec;
+    pub fn timespec_add(a: timespec, b: timespec) -> timespec;
+
+    pub fn current_column() -> Lisp_Object;
+
     pub fn Fadd_text_properties(
         start: Lisp_Object,
         end: Lisp_Object,
--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -65,6 +65,7 @@ mod cmds;
 mod data;
 mod fns;
 mod dispnew;
+mod indent;
 
 #[cfg(all(not(test), target_os = "macos"))]
 use alloc_unexecmacosx::OsxUnexecAlloc;
@@ -435,6 +436,7 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*data::Sindirect_function);
         defsubr(&*frames::Sselected_frame);
         defsubr(&*dispnew::Ssleep_for);
+        defsubr(&*indent::Scurrent_column);
         defsubr(&*process::Sprocess_list);
     }
 }
