b6650e4d3980dd343a7d46b39fd01b624706ff24
Óscar Nájera <najera.oscar@gmail.com>
2017-11-20 16:27:24 +0100

    Port process-sentinel

--- src/process.c
+++ src/process.c
@@ -1177,16 +1177,6 @@ It gets two arguments: the process, and a string describing the change.  */)
   return sentinel;
 }
 
-DEFUN ("process-sentinel", Fprocess_sentinel, Sprocess_sentinel,
-       1, 1, 0,
-       doc: /* Return the sentinel of PROCESS.
-See `set-process-sentinel' for more info on sentinels.  */)
-  (register Lisp_Object process)
-{
-  CHECK_PROCESS (process);
-  return XPROCESS (process)->sentinel;
-}
-
 DEFUN ("set-process-thread", Fset_process_thread, Sset_process_thread,
        2, 2, 0,
        doc: /* Set the locking thread of PROCESS to be THREAD.
@@ -7772,7 +7762,6 @@ The variable takes effect when `start-process' is called.  */);
   defsubr (&Sprocess_exit_status);
   defsubr (&Sset_process_filter);
   defsubr (&Sset_process_sentinel);
-  defsubr (&Sprocess_sentinel);
   defsubr (&Sset_process_thread);
   defsubr (&Sprocess_thread);
   defsubr (&Sset_process_window_size);
