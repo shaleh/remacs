c7094bd11f703d832f11a1d736ac7fdc16000646
Michael Albinus <michael.albinus@gmx.de>
2017-12-06 20:49:30 +0100

    Fix Bug#29579
    * lisp/files.el (file-name-non-special):
    Inhibit `file-name-handler-alist' only for some operations.
    Add missing operations.  (Bug#29579)
    
    * lisp/net/tramp-compat.el (tramp-compat-file-name-quote):
    Do not quote if it is quoted already.
    
    * lisp/net/tramp-smb.el (tramp-smb-handle-insert-directory):
    Use `copy-tree' but `copy-sequence'.
    
    * lisp/net/tramp.el (tramp-handle-file-truename): Handle several
    trailing slashes correctly.
    
    * test/lisp/net/tramp-tests.el (tramp-test11-copy-file)
    (tramp-test12-rename-file, tramp-test24-file-acl)
    (tramp-test25-file-selinux, tramp--test-check-files):
    Handle also quoted file names.
    (tramp-test21-file-links): Fix file name quoting test.
    (tramp-test24-file-acl): Be more robust for "smb" method.
    (tramp-test35-make-auto-save-file-name): Enable hidden test cases.

--- lisp/net/tramp.el
+++ lisp/net/tramp.el
@@ -3193,7 +3193,7 @@ User is always nil."
 	     (tramp-error
 	      v1 'file-error
 	      "Maximum number (%d) of symlinks exceeded" numchase-limit)))
-	 result))
+	 (directory-file-name result)))
 
      ;; Preserve trailing "/".
      (if (string-equal (file-name-nondirectory filename) "") "/" ""))))
