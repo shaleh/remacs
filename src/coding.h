2d076efdbabbfca82939b6bebb63c8090d8b3a12
Philipp Stephani <phst@google.com>
2017-12-23 17:56:36 +0100

    Improve error reporting when serializing non-Unicode strings to JSON
    * src/coding.c (utf8_string_p): New helper function.
    (syms_of_coding) <utf-8-unix>: Move from json.c.
    
    * src/json.c (json_check_utf8): New helper function.
    (lisp_to_json_toplevel_1, lisp_to_json): Use it.  To save a bit of
    time, check for invalid UTF-8 strings only after encountering an
    error, since Jansson already rejects them.
    
    * test/src/json-tests.el (json-serialize/invalid-unicode): Adapt
    expected error symbol.

--- src/coding.h
+++ src/coding.h
@@ -691,6 +691,7 @@ surrogates_to_codepoint (int low, int high)
 /* Extern declarations.  */
 extern Lisp_Object code_conversion_save (bool, bool);
 extern bool encode_coding_utf_8 (struct coding_system *);
+extern bool utf8_string_p (Lisp_Object);
 extern void setup_coding_system (Lisp_Object, struct coding_system *);
 extern Lisp_Object coding_charset_list (struct coding_system *);
 extern Lisp_Object coding_system_charset_list (Lisp_Object);
