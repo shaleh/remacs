90175a3c8d9eba83ddccfafb87fe43658763928d
Sean Perry <shaleh@speakeasy.net>
2018-01-25 19:29:31 -0800

    Port let and let* aka letX (#604)
    Add tests

--- rust_src/remacs-sys/lib.rs
+++ rust_src/remacs-sys/lib.rs
@@ -755,6 +755,10 @@ pub struct thread_state {
     // next_thread.
 }
 
+extern "C" {
+    pub fn SPECPDL_INDEX() -> ptrdiff_t;
+}
+
 /// Lisp_Char_Table
 #[repr(C)]
 #[allow(dead_code)]
@@ -1350,6 +1354,8 @@ extern "C" {
         object: Lisp_Object,
         overlay: *mut Lisp_Object,
     ) -> Lisp_Object;
+    pub fn specbind(symbol: Lisp_Object, value: Lisp_Object);
+    pub fn unbind_to(count: ptrdiff_t, value: Lisp_Object) -> Lisp_Object;
 
 }
 
