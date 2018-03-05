b6ef88f6364cc6946f1adaa64a1b78de2c53d50b
Glenn Morris <rgm@gnu.org>
2018-03-05 15:29:15 -0500

    Small auth-source-netrc-create fix
    * lisp/auth-source.el (auth-source-netrc-create):
    Fix handling of auth-source-netrc-use-gpg-tokens being a list.

--- lisp/auth-source.el
+++ lisp/auth-source.el
@@ -1318,9 +1318,7 @@ See `auth-source-search' for details on SPEC."
                                                    (string-match (car item) file))
                                            (setq ret (cdr item))
                                            (setq check nil)))
-                                       ;; FIXME: `ret' unused.
-                                       ;; Should we return it here?
-                                       ))
+                                       ret))
                                     (t 'never)))
                                   (plain (or (eval default) (read-passwd prompt))))
                              ;; ask if we don't know what to do (in which case
