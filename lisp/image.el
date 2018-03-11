3ee0f18188a871075685e1c4cbf5936d81843f75
Charles A. Roelli <charles@aurox.ch>
2018-03-11 11:13:47 +0100

    Improve and make use of 'image--get-image'
    * lisp/image.el (image--get-image): Add documentation, and
    check overlays for images too (since function 'put-image' from
    the same library uses overlays to insert images).
    (image-save): Use 'image--get-image'.

--- lisp/image.el
+++ lisp/image.el
@@ -981,7 +981,8 @@ default is 20%."
                         0.8)))
 
 (defun image--get-image ()
-  (let ((image (get-text-property (point) 'display)))
+  "Return the image at point."
+  (let ((image (get-char-property (point) 'display)))
     (unless (eq (car-safe image) 'image)
       (error "No image under point"))
     image))
@@ -1036,10 +1037,7 @@ default is 20%."
 (defun image-save ()
   "Save the image under point."
   (interactive)
-  (let ((image (get-text-property (point) 'display)))
-    (when (or (not (consp image))
-              (not (eq (car image) 'image)))
-      (error "No image under point"))
+  (let ((image (image--get-image)))
     (with-temp-buffer
       (let ((file (plist-get (cdr image) :file)))
         (if file
