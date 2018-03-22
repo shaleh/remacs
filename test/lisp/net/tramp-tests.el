42158fa2c18450bcaf64cfed23b4d4de4688d99d
Michael Albinus <michael.albinus@gmx.de>
2018-03-22 09:43:44 +0100

    Tag tramp-test39-utf8* as :unstable
    * test/lisp/net/tramp-tests.el (tramp-test39-utf8)
    (tramp-test39-utf8-with-stat, tramp-test39-utf8-with-perl)
    (tramp-test39-utf8-with-ls): Tag the tests as :unstable.

--- test/lisp/net/tramp-tests.el
+++ test/lisp/net/tramp-tests.el
@@ -4661,6 +4661,7 @@ Use the `ls' command."
 
 (ert-deftest tramp-test39-utf8 ()
   "Check UTF8 encoding in file names and file contents."
+  :tags '(:unstable)
   (skip-unless (tramp--test-enabled))
   (skip-unless (not (tramp--test-docker-p)))
   (skip-unless (not (tramp--test-rsync-p)))
@@ -4672,7 +4673,7 @@ Use the `ls' command."
 (ert-deftest tramp-test39-utf8-with-stat ()
   "Check UTF8 encoding in file names and file contents.
 Use the `stat' command."
-  :tags '(:expensive-test)
+  :tags '(:expensive-test :unstable)
   (skip-unless (tramp--test-enabled))
   (skip-unless (tramp--test-sh-p))
   (skip-unless (not (tramp--test-docker-p)))
@@ -4692,7 +4693,7 @@ Use the `stat' command."
 (ert-deftest tramp-test39-utf8-with-perl ()
   "Check UTF8 encoding in file names and file contents.
 Use the `perl' command."
-  :tags '(:expensive-test)
+  :tags '(:expensive-test :unstable)
   (skip-unless (tramp--test-enabled))
   (skip-unless (tramp--test-sh-p))
   (skip-unless (not (tramp--test-docker-p)))
@@ -4715,7 +4716,7 @@ Use the `perl' command."
 (ert-deftest tramp-test39-utf8-with-ls ()
   "Check UTF8 encoding in file names and file contents.
 Use the `ls' command."
-  :tags '(:expensive-test)
+  :tags '(:expensive-test :unstable)
   (skip-unless (tramp--test-enabled))
   (skip-unless (tramp--test-sh-p))
   (skip-unless (not (tramp--test-docker-p)))
