1ba555a028425eaeca49f4053ea6530fdf51568e
Daniel Brooks <db48x@db48x.net>
2018-07-27 16:38:43 -0700

    no longer any need for these accessors, now that we have bindgen

--- src/buffer.c
+++ src/buffer.c
@@ -5912,21 +5912,6 @@ keys_of_buffer (void)
   initial_define_key (control_x_map, 'k', "kill-buffer");
 }
 
-
-/* Accessors for Rust */
-
-struct Lisp_Overlay*
-bget_overlays_before(const struct buffer *b)
-{
-  return b->overlays_before;
-}
-
-struct Lisp_Overlay*
-bget_overlays_after(const struct buffer *b)
-{
-  return b->overlays_after;
-}
-
 extern void
 set_per_buffer_value (struct buffer *b, int offset, Lisp_Object value)
 {
