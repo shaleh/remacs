bff9c80c6cd320f9fbcd71243b641013f7a3b420
brotzeit <brotzeitmacher@gmail.com>
2018-10-24 20:30:24 +0200

    move test

--- rust_src/src/lisp.rs
+++ rust_src/src/lisp.rs
@@ -775,10 +775,3 @@ macro_rules! protect_statics_from_GC {
         }
     }
 }
-
-#[test]
-fn test_basic_float() {
-    let val = 8.0;
-    let result = mock_float!(val);
-    assert!(result.is_float() && result.as_float() == Some(val));
-}
