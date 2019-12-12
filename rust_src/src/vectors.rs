ec6021edeaf93b64675f487e3f93009ae2cb9ed0
Carlos Martín Nieto <cmn@dwim.me>
2019-12-12 23:29:21 +0000

    vectors: remove most transmute uses

--- rust_src/src/vectors.rs
+++ rust_src/src/vectors.rs
@@ -153,14 +153,14 @@ impl LispVectorlikeRef {
 
     pub fn as_vector(self) -> Option<LispVectorRef> {
         if self.is_vector() {
-            Some(unsafe { mem::transmute::<_, LispVectorRef>(self) })
+            Some(LispVectorRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
     }
 
     pub unsafe fn as_vector_unchecked(self) -> LispVectorRef {
-        mem::transmute::<_, LispVectorRef>(self)
+        LispVectorRef::new(self.as_ptr() as *mut _)
     }
 
     pub fn pseudovector_type(self) -> pvec_type {
@@ -184,7 +184,7 @@ impl LispVectorlikeRef {
 
     pub fn as_bool_vector(self) -> Option<LispBoolVecRef> {
         if self.is_pseudovector(pvec_type::PVEC_BOOL_VECTOR) {
-            Some(unsafe { mem::transmute::<_, LispBoolVecRef>(self) })
+            Some(LispBoolVecRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -192,7 +192,7 @@ impl LispVectorlikeRef {
 
     pub fn as_buffer(self) -> Option<LispBufferRef> {
         if self.is_pseudovector(pvec_type::PVEC_BUFFER) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispBufferRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -200,7 +200,7 @@ impl LispVectorlikeRef {
 
     pub fn as_subr(self) -> Option<LispSubrRef> {
         if self.is_pseudovector(pvec_type::PVEC_SUBR) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispSubrRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -208,7 +208,7 @@ impl LispVectorlikeRef {
 
     pub fn as_window(self) -> Option<LispWindowRef> {
         if self.is_pseudovector(pvec_type::PVEC_WINDOW) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispWindowRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -216,7 +216,7 @@ impl LispVectorlikeRef {
 
     pub fn as_window_configuration(self) -> Option<SaveWindowDataRef> {
         if self.is_pseudovector(pvec_type::PVEC_WINDOW_CONFIGURATION) {
-            Some(unsafe { mem::transmute(self) })
+            Some(SaveWindowDataRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -224,7 +224,7 @@ impl LispVectorlikeRef {
 
     pub fn as_frame(self) -> Option<LispFrameRef> {
         if self.is_pseudovector(pvec_type::PVEC_FRAME) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispFrameRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -232,7 +232,7 @@ impl LispVectorlikeRef {
 
     pub fn as_process(self) -> Option<LispProcessRef> {
         if self.is_pseudovector(pvec_type::PVEC_PROCESS) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispProcessRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -240,7 +240,7 @@ impl LispVectorlikeRef {
 
     pub fn as_thread(self) -> Option<ThreadStateRef> {
         if self.is_pseudovector(pvec_type::PVEC_THREAD) {
-            Some(unsafe { mem::transmute(self) })
+            Some(ThreadStateRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -256,7 +256,7 @@ impl LispVectorlikeRef {
 
     pub fn as_sub_char_table(self) -> Option<LispSubCharTableRef> {
         if self.is_pseudovector(pvec_type::PVEC_SUB_CHAR_TABLE) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispSubCharTableRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -272,7 +272,7 @@ impl LispVectorlikeRef {
 
     pub fn as_compiled(self) -> Option<LispVectorlikeSlotsRef> {
         if self.is_pseudovector(pvec_type::PVEC_COMPILED) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispVectorlikeSlotsRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -280,7 +280,7 @@ impl LispVectorlikeRef {
 
     pub fn as_record(self) -> Option<LispVectorlikeSlotsRef> {
         if self.is_pseudovector(pvec_type::PVEC_RECORD) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispVectorlikeSlotsRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
@@ -288,7 +288,7 @@ impl LispVectorlikeRef {
 
     pub fn as_font(self) -> Option<LispVectorlikeSlotsRef> {
         if self.is_pseudovector(pvec_type::PVEC_FONT) {
-            Some(unsafe { mem::transmute(self) })
+            Some(LispVectorlikeSlotsRef::new(self.as_ptr() as *mut _))
         } else {
             None
         }
